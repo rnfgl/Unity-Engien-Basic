85d0a07d57bfc98c31b894526a14bffafb8ee71d
KoTaeseong <rkdtnqlstx@gmail.com>
2023-02-18 15:03:31 +0900

    CSharp - Condition statements
    -if & switch-case statements
    -enum type

---
+++ CShap/Statement_SwithCase/GlobalDefintions.cs
@@ -0,0 +1,11 @@
+﻿// enum 키워드
+// 열거형 사용자 정의 자료형을 정의할때 쓰는 키워드
+*enum CharacterTypes
+{
+   None,//or Nothing or Idle
+    가렌 = 10
+    나서스,
+    다리우스,
+    바루스,
+    제드
+}
---
+++ CShap/Statment_If/Program.cs
@@ -0,0 +1,19 @@
+﻿bool condition1 = false;
+bool condition2 = true;
+
+if (condition1)
+{
+    //조건1이 참일때 실행할 내용
+    Console.WriteLine("조건1이 참이다");
+
+}
+else if (condition2)    
+{
+    //조건1이 거짓이고 조건2가 참일때 실행할 내용
+    Console.WriteLine("조건1이 거짓이고 조건2가 참이다");
+}
+else
+{
+    //위 조건들이 모두 거짓일때 실행할 내용
+    Console.WriteLine("조건 1, 조건 2 모두 거짓이다");
+}
